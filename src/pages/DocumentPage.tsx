690b2309c0ce6bfaca66ace7b72abb8758cfc14a
acezxn <acezxn@gmail.com>
2026-03-30 15:05:08 -0400

    Fixed warnings

--- src/pages/DocumentPage.tsx
+++ src/pages/DocumentPage.tsx
@@ -2,7 +2,7 @@ import { Document } from "@/components/ui/Document";
 import { Footer } from "@/components/ui/Footer";
 import { Navbar } from "@/components/ui/Navbar";
 import visible_documents from "@/data/visible_documents.json"
-import { Box, Heading } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { NoPage } from "./NoPage";
